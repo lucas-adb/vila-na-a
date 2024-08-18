bb17dbe215c8c77c5278c4e5a6430e513363d8d4
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-08-18 17:21:56 -0300

    adiciona loading svg

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import { usePollStore } from './stores/poll'
 
 const pollStore = usePollStore()
 const votes = ref({})
+const isLoading = ref(true)
 
 async function getPoll() {
   const { data, error } = await supabase.rpc('get_poll')
@@ -16,6 +17,7 @@ async function getPoll() {
 
   votes.value = data[0]
   console.log('poll', data)
+  isLoading.value = false
 }
 
 async function vote(bool) {
@@ -40,6 +42,8 @@ onMounted(() => {
   if (localStorage.getItem('vila-poll')) {
     getPoll()
     pollStore.setButtonClicked(true)
+  } else {
+    isLoading.value = false
   }
 })
 </script>
@@ -51,33 +55,38 @@ onMounted(() => {
     <p class="description">se passaram desde que o Vila Nova disputou a série A.</p>
   </div>
   <div class="poll">
-    <div class="poll__title">Esse ano sobe?</div>
-    <button v-if="!pollStore.isButtonClicked" class="poll__voting-btn" @click="() => vote(true)">
-      Sim
-    </button>
-    <button v-if="!pollStore.isButtonClicked" class="poll__voting-btn" @click="() => vote(false)">
-      Não
-    </button>
+    <img v-if="isLoading" src="./assets/loading.svg" alt="animated loading icon" class="loading" />
+    <template v-else>
+      <div class="poll__title">Esse ano sobe?</div>
+      <button v-if="!pollStore.isButtonClicked" class="poll__voting-btn" @click="() => vote(true)">
+        Sim
+      </button>
+      <button v-if="!pollStore.isButtonClicked" class="poll__voting-btn" @click="() => vote(false)">
+        Não
+      </button>
 
-    <div v-if="pollStore.isButtonClicked" class="poll__result">
-      <p>Sim</p>
-      <p>{{ ((votes.total_true / votes.total_votes) * 100).toFixed(2) }}%</p>
-      <div
-        class="poll__fill"
-        :style="{ width: ((votes.total_true / votes.total_votes) * 100).toFixed(2) + '%' }"
-      ></div>
-    </div>
+      <div v-if="pollStore.isButtonClicked" class="poll__result">
+        <p>Sim</p>
+        <p>{{ ((votes.total_true / votes.total_votes) * 100).toFixed(2) }}%</p>
+        <div
+          class="poll__fill"
+          :style="{ width: ((votes.total_true / votes.total_votes) * 100).toFixed(2) + '%' }"
+        ></div>
+      </div>
 
-    <div v-if="pollStore.isButtonClicked" class="poll__result">
-      <p>Não</p>
-      <p>{{ ((votes.total_false / votes.total_votes) * 100).toFixed(2) }}%</p>
-      <div
-        class="poll__fill"
-        :style="{ width: ((votes.total_false / votes.total_votes) * 100).toFixed(2) + '%' }"
-      ></div>
-    </div>
+      <div v-if="pollStore.isButtonClicked" class="poll__result">
+        <p>Não</p>
+        <p>{{ ((votes.total_false / votes.total_votes) * 100).toFixed(2) }}%</p>
+        <div
+          class="poll__fill"
+          :style="{ width: ((votes.total_false / votes.total_votes) * 100).toFixed(2) + '%' }"
+        ></div>
+      </div>
 
-    <p v-if="pollStore.isButtonClicked" class="poll__total-votes">{{ votes.total_votes }} votos</p>
+      <p v-if="pollStore.isButtonClicked" class="poll__total-votes">
+        {{ votes.total_votes }} votos
+      </p>
+    </template>
   </div>
   <a href="https://lucasalves.dev/" class="portfolio" target="_blank">lucasalves.dev</a>
 </template>
